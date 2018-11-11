7bbf9b611744134ec5c9648dd0da1b24f730a3d4
mjunior <mauriciojs.junior@gmail.com>
2018-11-11 18:52:58 -0200

    versioning API

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,7 @@ module DogwalkerApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.eager_load_paths << Rails.root.join('lib')
     config.middleware.use Rack::Attack
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :v1, path: "/" , constraints: ApiVersionConstraint.new(version: 1, default: true) do
+    #routes
+  end
 end
