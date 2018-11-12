b402480d2c0ad26a5464062234c280d3ba2d90f1
mjunior <mauriciojs.junior@gmail.com>
2018-11-12 04:14:00 -0200

    Testing Create DogWalking

---
+++ spec/requests/v1/dog_walking/dog_walking_create_errors_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.describe 'DogWalking index Api', type: :request do
+
+  describe 'GET /dog_walking' do
+    context "When invalid attributes" do
+      before do
+        Product.create({
+          name: 'Caminhada de 30 min',
+          duration: 30,
+          first_price: 25,
+          aditional_price: 15
+        })
+      end
+
+      it 'returns message errors messages' do
+        attrs = attributes_for(:dog_walking,schedule_date: Time.now + 1.hours, duration: 30, pets: 0 )
+        post '/dog_walkings', params: {dog_walking: attrs}
+        expect(json_body[:errors].nil?).to be_falsey  
+      end
+
+      it 'returns must be greater than 0' do
+        attrs = attributes_for(:dog_walking,schedule_date: Time.now + 1.hours, duration: 30, pets: 0 )
+        post '/dog_walkings', params: {dog_walking: attrs}
+        expect(json_body[:errors][:pets]).to include("must be greater than 0")  
+      end
+
+      it "returns schedule_date can't be blank" do
+        attrs = attributes_for(:dog_walking,schedule_date: nil, duration: 30, pets: 0 )
+        post '/dog_walkings', params: {dog_walking: attrs}
+        expect(json_body[:errors][:schedule_date]).to include("can't be blank")  
+      end
+
+      it "returns duration is not valid" do
+        attrs = attributes_for(:dog_walking,schedule_date: nil, duration: 32, pets: 0 )
+        post '/dog_walkings', params: {dog_walking: attrs}
+        expect(json_body[:errors][:duration]).to include("32.0 is not a valid duration")  
+      end
+    end
+  end
+end
---
+++ spec/requests/v1/dog_walking/dog_walking_create_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe 'DogWalking index Api', type: :request do
+
+  describe 'GET /dog_walking' do
+    context "When valid attributes" do
+      before do
+        Product.create({
+          name: 'Caminhada de 30 min',
+          duration: 30,
+          first_price: 25,
+          aditional_price: 15
+        })
+        attrs = attributes_for(:dog_walking,schedule_date: Time.now + 1.hours, duration: 30, pets: 1 )
+        post '/dog_walkings', params: {dog_walking: attrs}
+      end
+
+      it 'returns status :OK' do
+        expect(response).to have_http_status(:ok)
+      end
+      it 'returns a walk' do
+        expect(json_body[:data][:id].nil?).to be_falsey
+      end
+
+      it 'returns walk with correct status' do
+        expect(json_body[:data][:attributes][:status]).to eql('scheduled')
+      end
+
+      it 'returns walk with a price' do
+        expect(json_body[:data][:attributes][:price].nil?).to be_falsey
+      end
+    end
+  end
+end
